f21c726607a97cea9d97cb955e9aef0d3c1e8342
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2023-10-06 14:25:45 +0800

    feat: adding a state variable

--- src/adding-interactivity/state-a-components-memory/when-a-regular-variable-isnt-enough.js
+++ src/adding-interactivity/state-a-components-memory/when-a-regular-variable-isnt-enough.js
@@ -1,26 +1,39 @@
 import { useState } from 'react';
-import { sculptureList } from './data';
+import { sculptureList } from './data.js';
 
 export default function Gallery() {
   const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+  const hasNext = index < sculptureList.length - 1;
 
-  function handleClick() {
-    setIndex(index + 1);
+  function handleNextClick() {
+    if (hasNext) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
   }
 
   let sculpture = sculptureList[index];
   return (
     <>
-      <button onClick={handleClick}>Next</button>
+      <button onClick={handleNextClick}>Next</button>
       <h2>
-        <i>{sculpture.name}</i>
+        <i>{sculpture.name} </i>
         by {sculpture.artist}
       </h2>
       <h3>
         ({index + 1} of {sculptureList.length})
       </h3>
       <img src={sculpture.url} alt={sculpture.alt} />
-      <p>{sculpture.description}</p>
+      <button onClick={handleMoreClick}>
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
     </>
   );
 }
