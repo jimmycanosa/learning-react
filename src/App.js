8502e683faa6ab9a82706615d4545fd15fc1ef2b
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2023-10-06 18:57:24 +0800

    feat: implement a traffic light

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import MyApp from './adding-interactivity/state-as-a-snapshot/state-over-time.js';
+import MyApp from './adding-interactivity/state-as-a-snapshot/implement-a-traffic-light.js';
 
 export default function App() {
   return (
