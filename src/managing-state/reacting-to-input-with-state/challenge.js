f612e512a16c9b458e3a488eb6ab8fcf4b2c760c
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2023-10-11 21:16:12 +0800

    fix: add and remove a css class

--- src/managing-state/reacting-to-input-with-state/challenge.js
+++ src/managing-state/reacting-to-input-with-state/challenge.js
@@ -1,8 +1,24 @@
+import { useState } from 'react';
+
 export default function Picture() {
+  const [isActive, setIsActive] = useState(false);
+
+  let divClassName = 'background';
+  let imgClassName = 'picture';
+  if (isActive) {
+    imgClassName += ' picture--active';
+  } else {
+    divClassName += ' background--active';
+  }
+
   return (
-    <div className='background background--active'>
+    <div className={divClassName} onClick={() => setIsActive(false)}>
       <img
-        className='picture'
+        onClick={(e) => {
+          e.stopPropagation();
+          setIsActive(true);
+        }}
+        className={imgClassName}
         alt='Rainbow houses in Kampung Pelangi, Indonesia'
         src='https://i.imgur.com/5qwVYb1.jpeg'
       />
