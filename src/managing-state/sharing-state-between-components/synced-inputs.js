6b39fcd43a9ad6050132b7e7bab8fea99863015a
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2023-10-14 11:48:10 +0800

    fix: synced inputs

--- src/managing-state/sharing-state-between-components/synced-inputs.js
+++ src/managing-state/sharing-state-between-components/synced-inputs.js
@@ -1,15 +1,6 @@
 import { useState } from 'react';
 
 export default function SyncedInputs() {
-  return (
-    <>
-      <Input label='First input' />
-      <Input label='Second input' />
-    </>
-  );
-}
-
-function Input({ label }) {
   const [text, setText] = useState('');
 
   function handleChange(e) {
@@ -17,8 +8,17 @@ function Input({ label }) {
   }
 
   return (
+    <>
+      <Input label='First input' value={text} onChange={handleChange} />
+      <Input label='Second input' value={text} onChange={handleChange} />
+    </>
+  );
+}
+
+function Input({ label, value, onChange }) {
+  return (
     <label>
-      {label} <input value={text} onChange={handleChange} />
+      {label} <input value={value} onChange={onChange} />
     </label>
   );
 }
