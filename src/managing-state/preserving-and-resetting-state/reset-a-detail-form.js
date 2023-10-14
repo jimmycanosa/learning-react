977fecdd187bda6dfcb65ee7e214e3456f8dade5
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2023-10-14 21:07:47 +0800

    fix: reset a detail form

--- src/managing-state/preserving-and-resetting-state/reset-a-detail-form.js
+++ src/managing-state/preserving-and-resetting-state/reset-a-detail-form.js
@@ -26,7 +26,11 @@ export default function ContactManager() {
         onSelect={(id) => setSelectedId(id)}
       />
       <hr />
-      <EditContact initialData={selectedContact} onSave={handleSave} />
+      <EditContact
+        key={selectedId}
+        initialData={selectedContact}
+        onSave={handleSave}
+      />
     </div>
   );
 }
