7998fb7ffe849339ef4a79d33616b0fd13594134
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2023-10-18 12:13:06 +0800

    fix: context - an alternative to passing props

--- src/managing-state/passing-data-deeply-with-context/Section.js
+++ src/managing-state/passing-data-deeply-with-context/Section.js
@@ -1,3 +1,9 @@
-export default function Section({ children }) {
-  return <section className='section'>{children}</section>;
+import { LevelContext } from './LevelContext';
+
+export default function Section({ level, children }) {
+  return (
+    <section className='section'>
+      <LevelContext.Provider value={level}>{children}</LevelContext.Provider>
+    </section>
+  );
 }
