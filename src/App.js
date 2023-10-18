b100e3bd96648f24811f91ac4c43d7262ae35e58
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2023-10-18 14:25:39 +0800

    fix: replace prop drilling with context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import MyApp from './managing-state/passing-data-deeply-with-context/context-passes-through-intermediate-components.js'
+import MyApp from './managing-state/passing-data-deeply-with-context/replace-prop-drilling-with-context.js'
 
 export default function App() {
   return (
