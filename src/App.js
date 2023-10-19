d053455aa141b787e60bddc293a08dfe1b595ef8
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2023-10-19 19:10:38 +0800

    feat: fix a component failing to re-render

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import MyApp from './escape-hatches/referencing-values-with-refs/fix-a-broken-chat-input.js';
+import MyApp from './escape-hatches/referencing-values-with-refs/fix-a-component-failing-to-re-render.js';
 
 export default function App() {
   return (
