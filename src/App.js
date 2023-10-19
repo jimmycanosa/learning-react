6183a5d3e2ba5b2578a3400eb4b5f8eeb07458e2
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2023-10-19 10:59:54 +0800

    feat: synchronizing with effects

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import MyApp from './escape-hatches/manipulating-the-dom-with-refs.js';
+import MyApp from './escape-hatches/synchronizing-with-effects.js';
 
 export default function App() {
   return (
