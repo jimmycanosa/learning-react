cc9c03ad6797e44ddac6c76c0fae2c74cc027a69
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2023-10-19 20:03:20 +0800

    fix: read the latest state

--- src/escape-hatches/referencing-values-with-refs/read-the-latest-state.js
+++ src/escape-hatches/referencing-values-with-refs/read-the-latest-state.js
@@ -2,16 +2,22 @@ import { useState, useRef } from 'react';
 
 export default function Chat() {
   const [text, setText] = useState('');
+  const textRef = useRef(text);
+
+  function handleChange(e) {
+    setText(e.target.value);
+    textRef.current = e.target.value;
+  }
 
   function handleSend() {
     setTimeout(() => {
-      alert('Sending: ' + text);
+      alert('Sending: ' + textRef.current);
     }, 3000);
   }
 
   return (
     <>
-      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <input value={text} onChange={handleChange} />
       <button onClick={handleSend}>Send</button>
     </>
   );
