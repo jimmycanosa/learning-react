fb36a76191098aff3ff6930722ddc5956b26616c
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2023-10-24 15:04:59 +0800

    feat: populate a chain of select boxes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import MyApp from './escape-hatches/lifecycle-of-reactive-effects/connection-switch.js';
+import MyApp from './escape-hatches/lifecycle-of-reactive-effects/populate-a-chain-of-select-boxes.js';
 
 export default function App() {
   return (
