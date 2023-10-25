7fe073b70370f820b7daa258f63dce49d8250c3a
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2023-10-25 10:14:21 +0800

    fix: resetting interval

--- src/escape-hatches/removing-effect-dependencies/resetting_interval.js
+++ src/escape-hatches/removing-effect-dependencies/resetting_interval.js
@@ -7,13 +7,13 @@ export default function Timer() {
     console.log('✅ Creating an interval');
     const id = setInterval(() => {
       console.log('⏰ Interval tick');
-      setCount(count + 1);
+      setCount(c => c + 1);
     }, 1000);
     return () => {
       console.log('❌ Clearing an interval');
       clearInterval(id);
     };
-  }, [count]);
+  }, []);
 
   return <h1>Counter: {count}</h1>;
 }
