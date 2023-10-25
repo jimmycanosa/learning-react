527ab2224bb815a25ef3b695f7eeecbd92af3707
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2023-10-25 16:01:47 +0800

    feat: share stateful logic not state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import MyApp from './escape-hatches/reusing-logic-with-custom-hooks/sharing-logic-between-components.js';
+import MyApp from './escape-hatches/reusing-logic-with-custom-hooks/share-statefull-logic-not-state-itself.js';
 
 export default function App() {
   return (
