28089a6414c3ae82153c8e0c8c65028d8e93ea09
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2023-10-25 17:49:36 +0800

    feat: implement a staggering movement

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import MyApp from './escape-hatches/reusing-logic-with-custom-hooks/extract-useinterval-out-of-usecounter.js';
+import MyApp from './escape-hatches/reusing-logic-with-custom-hooks/implement-a-staggering-movement.js';
 
 export default function App() {
   return (
