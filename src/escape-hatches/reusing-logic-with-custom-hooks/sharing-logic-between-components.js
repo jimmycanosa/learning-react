a120751b25973b173650b24d159969a8cb3d08f5
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2023-10-25 15:31:39 +0800

    feat: extracting you own custom Hook fr component

--- src/escape-hatches/reusing-logic-with-custom-hooks/sharing-logic-between-components.js
+++ src/escape-hatches/reusing-logic-with-custom-hooks/sharing-logic-between-components.js
@@ -1,21 +1,29 @@
-import { useState, useEffect } from 'react';
+import { useOnlineStatus } from './useOnlineStatus.js';
 
-export default function StatusBar() {
-  const [isOnline, setIsOnline] = useState(true);
-  useEffect(() => {
-    function handleOnline() {
-      setIsOnline(true);
-    }
-    function handleOffline() {
-      setIsOnline(false);
-    }
-    window.addEventListener('online', handleOnline);
-    window.addEventListener('offline', handleOffline);
-    return () => {
-      window.removeEventListener('online', handleOnline);
-      window.removeEventListener('offline', handleOffline);
-    };
-  }, []);
-
+function StatusBar() {
+  const isOnline = useOnlineStatus();
   return <h1>{isOnline ? '✅ Online' : '❌ Disconnected'}</h1>;
+}
+
+function SaveButton() {
+  const isOnline = useOnlineStatus();
+
+  function handleSaveClick() {
+    console.log('✅ Progress saved');
+  }
+
+  return (
+    <button disabled={!isOnline} onClick={handleSaveClick}>
+      {isOnline ? 'Save progress' : 'Reconnecting...'}
+    </button>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <SaveButton />
+      <StatusBar />
+    </>
+  );
 }
